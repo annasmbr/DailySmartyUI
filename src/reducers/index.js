7c5c75c3436030c1798e9ff297590ab5facdf8c8
annasmbr <donanna77@hotmail.com>
2020-11-14 21:29:26 +0100

    set up redux forum in searchbar.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 
+import { reducer as form } from 'redux-form';
+
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  form
+  //state: (state = {}) => state
 });
 
 export default rootReducer;

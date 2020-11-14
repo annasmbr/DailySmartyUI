7a33ce3dd41753f80f6f9dc4cadc034ec2f5b6d0
annasmbr <donanna77@hotmail.com>
2020-11-14 20:57:40 +0100

    generated project

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Logo</h1>
+      </div>
+    );
+  }
+}
+
+//<h2>React Redux Router</h2>

313f1014a5c591b8de4cfa62831ce96e94e5f092
annasmbr <donanna77@hotmail.com>
2020-11-14 21:16:18 +0100

    created recent posts and search bar components and used them in the home component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+
 import Logo from './logo';
+import SearchBar from './searchBar';
+import RecentPosts from './recentPosts';
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <Logo/>
+      <div>
+        <div>
+          <Logo/>
+          <SearchBar/>
+          <RecentPosts/>
+        </div>
       </div>
     );
   }
@@ -13,3 +20,4 @@ export default class App extends Component {
 
 //<h2>React Redux Router</h2>
 //<h1>Logo</h1>
+//<div className='app'>
